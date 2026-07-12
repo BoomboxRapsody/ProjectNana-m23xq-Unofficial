9580a7c4e9c41b03ab58425c01ef9a595e437aa5
BoomboxRapsody <seonghyeon802@gmail.com>
2026-07-12 19:12:22 +0900

    Update sixteen.sh

--- sixteen.sh
+++ sixteen.sh
@@ -26,7 +26,7 @@ export BUILD_PARTITIONS="product,system_ext,system"
 
 if [ "$STOCK_DEVICE" != "None" ]; then
     if curl -fsSL \
-        "https://api.github.com/repos/SN-Abdullah-Al-Noman/QuantumROM/releases/tags/QuantumROM_Devices" |
+        "https://api.github.com/repos/BoomboxRapsody/ProjectNana-m23xq-Unofficial/releases/tags/ProjectNana_Devices" |
         jq -e --arg dev "${STOCK_DEVICE}.zip" '.assets[].name == $dev' |
         grep -q true; then
         echo "$STOCK_DEVICE is supported"
@@ -40,7 +40,7 @@ fi
 if [ ! -f "$(pwd)/QuantumROM/Devices/${STOCK_DEVICE}.zip" ]; then
     if curl -fsSL --connect-timeout 5 https://www.google.com >/dev/null; then
         wget --no-check-certificate \
-            "https://github.com/SN-Abdullah-Al-Noman/QuantumROM/releases/download/QuantumROM_Devices/${STOCK_DEVICE}.zip" \
+            "https://api.github.com/repos/BoomboxRapsody/ProjectNana-m23xq-Unofficial/releases/download/ProjectNana_Devices/${STOCK_DEVICE}.zip" \
             -O "$(pwd)/QuantumROM/Devices/${STOCK_DEVICE}.zip"
     else
 	    rm -rf "$(pwd)/QuantumROM/Devices/${STOCK_DEVICE}.zip"
